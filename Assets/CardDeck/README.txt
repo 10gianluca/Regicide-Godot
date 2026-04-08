dbf65da5eebec31ef292067a998357dca92fb09f
10gianluca <gciccone01@gmail.com>
2026-04-08 05:10:32 -0400

    Card deck added

---
+++ Assets/CardDeck/README.txt
@@ -0,0 +1,17 @@
+# Minimal SVG Card Deck
+
+This package contains an original minimalist 52-card deck in SVG format, plus one optional card back.
+
+Files:
+- AS.svg ... KS.svg = spades
+- AH.svg ... KH.svg = hearts
+- AD.svg ... KD.svg = diamonds
+- AC.svg ... KC.svg = clubs
+- BACK.svg = optional card back
+
+Canvas size:
+- 750 × 1050 px
+
+Notes:
+- Face cards are minimalist monogram designs rather than illustrated royals.
+- SVGs use generic serif text for rank letters/numbers and custom vector suit shapes.
---
+++ Scripts/deck.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+const CARD_SCENE_PATH = "res://Scenes/card.tscn"
+const CARD_DRAWN_SPEED = 0.5
+
+var player_deck = ["2D", "2S", "2H", "2D", "2C"]
+var card_database_reference 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	player_deck.shuffle()
+	$RichTextLabel.text = str(player_deck.size())
+	card_database_reference = preload("res://Scripts/CardDatabase.gd")
+
+func draw_card():
+	var card_drawn_name = player_deck[0]
+	player_deck.erase(card_drawn_name)
+	
+	if player_deck.size() == 0:
+		$Area2D/CollisionShape2D.disabled = true
+		$Sprite2D.visible = false
+		$RichTextLabel.visible = false
+	$RichTextLabel.text = str(player_deck.size())
+	var card_scene = preload(CARD_SCENE_PATH)
+	var new_card = card_scene.instantiate()
+	var card_image_path = str("res://Assets/CardDeck/" + card_drawn_name + ".svg")
+	new_card.get_node("CardImage").texture = load(card_image_path)
+	new_card.get_node("Attack").text = str(card_database_reference.CARDS[card_drawn_name][0])
+	new_card.get_node("Health").text = str(card_database_reference.CARDS[card_drawn_name][1])
+	$"../CardManager".add_child(new_card)
+	new_card.name = "Card"
+	$"../PlayerHand".add_card_to_hand(new_card, CARD_DRAWN_SPEED)
+	print(card_image_path)
